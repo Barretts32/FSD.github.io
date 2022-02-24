b25b00b15509e9afd1b37a752cea4e5cec022c1e
Barretts32 <barrett.sharpe1005@gmail.com>
2022-02-24 16:01:29 +0000

    added features to runtime

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,8 +26,8 @@ var background = function (window) {
         var background;
         
         // ANIMATION VARIABLES HERE:
-        
-     
+        var tree; //defines the tree variable
+        var buildings = []; //defines the buildings variable to an empty array
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -36,17 +36,38 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
-            background.addChild(backgroundFill);
+            var backgroundFill = draw.rect(canvasWidth,groundY,'grey');//sets the backgroundfill variable to a rectangle that has a length height and color
+            background.addChild(backgroundFill); //pushes the variable storing the background to the background element
             
             // TODO: 3 - Add a moon and starfield
-            
+            for(let i = 0; i < 100; i++){ //defines a for loop that will run 100 times
+                var circle = draw.circle(10,'white','LightGray',2); //draws a circle and defines the varaible circle to it
+                circle.x = canvasWidth*Math.random(); //sets the location of the circle to a random position on the canvas x
+                circle.y = groundY*Math.random() -50; //sets the location of the cirlce to a random position above the ground plus 50
+                background.addChild(circle); //adds the circle variable to the background element
+            }
+            var moon = draw.bitmap('img/moon.png'); //defines the moon variable as the stored image
+            moon.x = canvasWidth - 450; //sets the x loc of the moon to the canvas width - 450 
+            moon.y =  groundY - 450; //sets the y loc of the moon to the ground height plus 450
+            moon.scaleX = 1; //sets the scale to 1
+            moon.scaleY = 1; //sets the scale to 1
+            background.addChild(moon); //adds the variable storing the moon to the background element
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            for(var i=0;i<5;++i) { //creates a loop that will run 5 times
+                var buildingHeight = 300 * Math.random() + 100; //sets a variable to building height
+                var building = draw.rect(75,buildingHeight,'lightgrey','Black',1);// defines a variable named building that is set to the return of a rect draw function
+                building.x = 200*i; //sets the location of the building to 200 pixels multiplicative of the instance of the for loop it is on
+                building.y = groundY-buildingHeight; //sets the y loc of the building to the ground plus the building height
+                background.addChild(building); //adds the building to the background element
+                buildings.push(building); //pushes the building to the buildings array
+            }
             
             // TODO 4: Part 1 - Add a tree
-            
+            tree = draw.bitmap('img/tree.png'); //defines tree as the stroed image
+            tree.x = canvasWidth -1700; //sets the tree x loc to the canvas width - 1700
+            tree.y = groundY - 250; //sets the tree y to the ground height plus 250
+            background.addChild(tree); //adds the tree variable to the background element
             
         } // end of render function - DO NOT DELETE
         
@@ -60,9 +81,15 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 4: Part 2 - Move the tree!
-            
+            tree.x = tree.x - 1; //everytime the update function is called the tree x loc is set to itself - 1 to animate it to move to the left
+            if(tree.x < -200) { //if the tree passes the left of the screen by 200 pixels condition
+                tree.x = canvasWidth; //sets the tree back to the right of the canvas
+            }
             
             // TODO 5: Part 2 - Parallax
+            for (var i = 0; i < buildings.length; i++) { //creates a for loop that will run the number of indexes in the building array
+                buildings[i].x -= 0.1; // updates the builing x pos to .1 to the left
+            }
             
 
         } // end of update function - DO NOT DELETE
