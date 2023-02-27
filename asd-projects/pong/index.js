71dbe1a297f4bde8048831fafd7b7f7d27cbf332
Barretts32 <barrett.sharpe1005@gmail.com>
2023-02-27 17:35:09 +0000

    First commit

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -11,12 +11,80 @@ function runProgram(){
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
-  // Game Item Objects
 
+  var gameObjects = {}
+  // Game Item Objects
+  function createObject(id, func){
+    var $object = $(id);
+    $object['y'] = parseInt($object.css('top'));
+    $object['x'] = parseInt($object.css('left'));
+    $object['rot'] = parseInt($object.css('rotate'));
+    for(var i = 0; i < arguments.length; i++){
+      if((typeof arguments[i]) == 'function'){
+        $object[arguments[i].name] = arguments[i]
+      }
+    }
+    gameObjects[id] = $object;
+  }
 
   // one-time setup
+  function move(direction, obj){
+    switch(direction){
+      case 'w':
+        gameObjects['#leftPaddle'].y - (1 * speedScalar)
+        break;
+      case 'd':
+        gameObjects['#leftPaddle'].rot + (1 * speedScalar)
+        break;
+      case 'a':
+        gameObjects['#leftPaddle'].rot--
+        break;
+      case 's':
+        gameObjects['#leftPaddle'].y++
+        break;
+      case 'ArrowUp':
+        gameObjects['#rightPaddle'].y--
+        break;
+      case 'ArrowRight':
+        gameObjects['#rightPaddle'].rot++
+        break;
+      case 'ArrowLeft':
+        gameObjects['#rightPaddle'].rot--
+        break;
+      case 'ArrowDown':
+        gameObjects['#rightPaddle'].y++
+        break;
+      case 'up':
+        obj.y--
+        break;
+      case 'down':
+        obj.y++
+        break;
+      case 'left':
+        obj.x--
+        break;
+      case 'right':
+        obj.x++
+        break;
+      default:
+        return
+    }     
+  }
+
+  function update(obj){
+    for(var p in obj){
+      $(p).css('top', obj[p].y)
+      $(p).css('left', obj[p].x)
+      $(p).css('rotate', obj[p].rot)
+    }
+  }
+
+
+  createObject('#ball');
+  createObject('#leftPaddle');
+  createObject('#rightPaddle');
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleEvent);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,7 +95,7 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    update(gameObjects)
 
   }
   
@@ -35,7 +103,7 @@ function runProgram(){
   Called in response to events.
   */
   function handleEvent(event) {
-
+    move(event.key);
   }
 
   ////////////////////////////////////////////////////////////////////////////////
